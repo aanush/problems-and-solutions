e18c6d28c6da4a999fe0fd820a6cd5a780114f37
Anish Anushang <anish.anushang@outlook.com>
2018-05-14 03:20:48 +0530

    added new project : compete

---
+++ compete/src/main/java/com/student/compete/CompeteApplication.java
@@ -0,0 +1,8 @@
+package com.student.compete;
+
+public class CompeteApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ compete/src/test/java/com/student/compete/CompeteApplicationTests.java
@@ -0,0 +1,8 @@
+package com.student.compete;
+
+public class CompeteApplicationTests {
+
+    public void contextLoads() {
+    }
+
+}
